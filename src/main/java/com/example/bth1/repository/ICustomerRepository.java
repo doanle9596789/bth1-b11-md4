292bfc5421d2abbf5c6a7dd80b8c0d485af60a89
doanle95956789 <doanle95956@gmail.com>
2023-04-05 13:49:35 +0700

    bth1-b11-md4

---
+++ src/main/java/com/example/bth1/repository/ICustomerRepository.java
@@ -0,0 +1,10 @@
+package com.example.bth1.repository;
+
+;
+import com.example.bth1.model.Customer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICustomerRepository extends PagingAndSortingRepository<Customer, Long> {
+}
---
+++ src/main/java/com/example/bth1/service/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.bth1.service;
+
+
+import com.example.bth1.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
